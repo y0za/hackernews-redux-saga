514da72c5db891b787f55b51e9650111e9c1170a
yoza <yoza.is12s@gmail.com>
2016-08-07 23:47:08 +0900

    Add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,43 @@
 import { combineReducers } from 'redux'
+import * as actions from '../actions'
 
-function app(state = {}, action) {
-  return state
+const loadItemsLimit = 10
+
+const initialState = {
+  app: {
+    loading: false,
+    items: []
+  },
+  nextPage: {
+    start: 0,
+    end: loadItemsLimit
+  }
+}
+
+function app(state = initialState.app, { type, payload }) {
+  switch (type) {
+    case actions.REQUEST_TOP_STORIES:
+      return { ...state, loading: true }
+    case actions.RECEIVE_TOP_STORIES:
+      return { loading: false, items: [...state.items, ...payload] }
+    default:
+      return state
+  }
+}
+
+function nextPage(state = initialState.nextPage, { type }) {
+  switch (type) {
+    case actions.RECEIVE_TOP_STORIES:
+      return {
+        start: state.end,
+        end: state.end + loadItemsLimit
+      }
+    default:
+      return state
+  }
 }
 
 export default combineReducers({
-  app
+  app,
+  nextPage
 })
