611d3020eda720e1f09bf528b543138e9acd54de
yoza <yoza.is12s@gmail.com>
2016-08-07 23:47:58 +0900

    Add sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,2 +1,20 @@
+import { take, put, call, select, fork } from 'redux-saga/effects'
+import * as actions from '../actions'
+import * as api from '../api'
+
+export function* fetchTopStories() {
+  while (true) {
+    const { start, end } = yield select(state => state.nextPage)
+    yield take(actions.REQUEST_TOP_STORIES)
+
+    put(actions.requestTopStories())
+    const ids = yield call(api.fetchTopStories, start, end)
+    const items = yield call(api.fetchItems, ids)
+    console.log(items)
+    yield put(actions.receiveTopStories(items))
+  }
+}
+
 export default function* rootSaga() {
+  yield fork(fetchTopStories)
 }
